5c5725a756e59f82404d1a660692a96f81a60c1b
qmccloud <quinmccl@udel.edu>
2020-09-24 18:49:25 -0400

    Minor semantic changes

--- sudokuLab/src/main/java/pkgHelper/LatinSquare.java
+++ sudokuLab/src/main/java/pkgHelper/LatinSquare.java
@@ -1,5 +1,7 @@
 package pkgHelper;
 
+import java.util.Arrays;
+
 public class LatinSquare {
 
 	private int[][] LatinSquare;
@@ -21,24 +23,23 @@ public class LatinSquare {
 		this.LatinSquare = s;
 	}
 
-	static boolean hasDuplicates(int[] arr) {
-		for (int i = 0; i < arr.length -1; i++)
-			for (int j = i+1; j < arr.length; j++)
-				if (arr[i] == arr[j])
-					return true;
-		return false;
-	}
-
-	static boolean doesElementExist(int[] arr, int iValue) {
-		for (int i = 0; i < arr.length; i++)
-			if (arr[i] == iValue)
+	boolean hasDuplicates(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++)
+			if (this.doesElementExist(Arrays.copyOfRange(arr, i + 1, arr.length), arr[i]))
 				return true;
 		return false;
 	}
 
-	static boolean hasAllValues(int[] arr1, int[] arr2) {
-		for (int i = 0; i < arr2.length; i++)
-			if (!doesElementExist(arr1, arr2[i]))
+	boolean doesElementExist(int[] arr, int iValue) {
+		for (int i : arr)
+			if (i == iValue)
+				return true;
+		return false;
+	}
+
+	boolean hasAllValues(int[] arr1, int[] arr2) {
+		for (int i : arr2)
+			if (!this.doesElementExist(arr1, i))
 				return false;
 		return true;
 	}
@@ -56,21 +57,20 @@ public class LatinSquare {
 
 	boolean isLatinSquare() {
 		for (int i = 0; i < this.LatinSquare.length; i++) {
-			if (hasDuplicates(this.getColumn(i)) || hasDuplicates(this.getRow(i)))
+			if (this.hasDuplicates(this.getColumn(i)) || this.hasDuplicates(this.getRow(i)))
 				return false;
-			for (int j = 0; j < this.LatinSquare.length; j++)
-				if (!hasAllValues(this.getRow(i), this.getRow(j))
-						|| !hasAllValues(this.getColumn(i), this.getColumn(j)))
+			for (int j = i; j < this.LatinSquare.length; j++)
+				if (!this.hasAllValues(this.getRow(i), this.getRow(j))
+						|| !this.hasAllValues(this.getColumn(i), this.getColumn(j)))
 					return false;
 		}
 		return true;
 	}
 
 	public boolean containsZero() {
-		for (int i = 0; i < this.LatinSquare.length; i++)
-			for (int j = 0; j < this.LatinSquare[i].length; j++)
-				if (this.LatinSquare[i][j] == 0)
-					return true;
+		for (int[] i : this.LatinSquare)
+			if (this.doesElementExist(i, 0))
+				return true;
 		return false;
 	}
 }
